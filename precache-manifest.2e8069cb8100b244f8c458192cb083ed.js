0aaa11b39ecc37a58af46d11df545dc0eefad875
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-07-24 12:30:31 +0530

    Updates

---
+++ precache-manifest.2e8069cb8100b244f8c458192cb083ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fb878e5855ad05d6a4682e215ef3b8",
+    "url": "/tracker5/index.html"
+  },
+  {
+    "revision": "4a5fd9632d9ce2ef2cf9",
+    "url": "/tracker5/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e52f5ffe292c34082509",
+    "url": "/tracker5/static/css/main.b7ca3098.chunk.css"
+  },
+  {
+    "revision": "4a5fd9632d9ce2ef2cf9",
+    "url": "/tracker5/static/js/2.addf8afa.chunk.js"
+  },
+  {
+    "revision": "7160b2f5853bf8c88805747cb6fce66f",
+    "url": "/tracker5/static/js/2.addf8afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52f5ffe292c34082509",
+    "url": "/tracker5/static/js/main.eeaee33c.chunk.js"
+  },
+  {
+    "revision": "cfef49663f80c1bc448a",
+    "url": "/tracker5/static/js/runtime-main.b5683459.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tracker5/static/media/logo.5d5d9eef.svg"
+  }
+]);
